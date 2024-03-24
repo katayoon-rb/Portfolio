31f388e6f7de4766269bd998f968e8902a0fb545
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-24 21:30:07 +0330

    Update SkillItem.jsx

--- src/components/SkillItem.jsx
+++ src/components/SkillItem.jsx
@@ -1,11 +1,10 @@
 function SkillItem({name, state}) {
     return (
-        <div className="bg-white px-10 py-8 rounded-lg text-center
+        <div className="bg-white md:px-10 py-8 rounded-lg text-center
                 w-[40%] md:w-fit md:min-w-[150px]">
             <div className="font-Truculenta font-bold text-green text-lg md:text-2xl">
                 {name}
             </div>
-
             <div className="font-medium text-gray-dark text-base md:text-md">
                 {state}
             </div>
