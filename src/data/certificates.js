6802865a479d69b9e5a15f1d5798cf4d9d5037eb
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-24 21:20:50 +0330

    Update certificates.js

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,18 +1,18 @@
 let certificatesArr = [
     {
-        'name': 'CS50x Harvard 2023',
+        'name': 'CS50x Harvard',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/CS50x.png?raw=true',
     },
     {
-        'name': 'CS50p Harvard 2023',
+        'name': 'CS50p Harvard',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/CS50p.png?raw=true',
     },
     {
-        'name': '50 Projects In 50 Days Udemy 2023',
+        'name': '50 Projects In 50 Days',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/50%20Projects%20In%2050%20Days.jpg?raw=true',
     },
     {
-        'name': 'NodeJs Udemy 2023',
+        'name': 'NodeJs Udemy',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/NodeJs.jpg?raw=true',
     },
 ]
