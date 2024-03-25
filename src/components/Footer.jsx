aec05c4a6ae4ff5c2c977df760fd01a735d9d14e
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-25 13:19:56 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className='w-full flex py-20 bg-green'>
       <div className='flex justify-center items-center gap-5 mx-auto'>
         {contactArr.map(way => (
-          <a href={way.link}>
+          <a href={way.link} key={way.key}>
             <svg xmlns='http://www.w3.org/2000/svg' viewBox={way.viewBox}
                 className='w-[50px] h-[50px] fill-light'>
               <path d={way.d}/>
