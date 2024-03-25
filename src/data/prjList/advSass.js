5566af4204c572cd19f0cfee57398981941cab98
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-25 14:16:14 +0100

    Add files via upload

---
+++ src/data/prjList/advSass.js
@@ -0,0 +1,19 @@
+const advSassPrj = [
+    {
+        'name': 'Nexter',
+        'link': 'https://katyadvcssandsass.netlify.app/nexter/',
+        'lan': ['html', 'css', 'sass']
+    },
+    {
+        'name': 'Trillo',
+        'link': 'https://katyadvcssandsass.netlify.app/trillo/',
+        'lan': ['html', 'css', 'sass']
+    },
+    {
+        'name': 'Natours',
+        'link': 'https://katyadvcssandsass.netlify.app/natours/',
+        'lan': ['html', 'css', 'sass']
+    }
+]
+
+export default advSassPrj
---
+++ src/data/prjList/twScratch.js
@@ -0,0 +1,44 @@
+const twScratchPrj = [
+    {
+        'name': 'Sandbox',
+        'link': 'https://katytailwindfromscratch.netlify.app/00%20Sandbox/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Joy of React Mini Projects',
+        'link': 'https://katytailwindfromscratch.netlify.app/05%20-%20Mini-Projects/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Clipboard Website',
+        'link': 'https://katytailwindfromscratch.netlify.app/06%20-%20Clipboard%20Website/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'LoopStudios Website',
+        'link': 'https://katytailwindfromscratch.netlify.app/07%20-%20Loopstudios%20Website/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Shortly Website',
+        'link': 'https://katytailwindfromscratch.netlify.app/08%20-%20Shortly%20Website/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Testimonial Grid',
+        'link': 'https://katytailwindfromscratch.netlify.app/09%20-%20Testimonial%20Grid%20Project/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Fylo Website',
+        'link': 'https://katytailwindfromscratch.netlify.app/10%20-%20Fylo%20Website/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+    {
+        'name': 'Bookmark Manager Website',
+        'link': 'https://katytailwindfromscratch.netlify.app/11%20-%20Bookmark%20Manager%20Website/index.html',
+        'lan': ['html', 'css', 'javascript', 'tailwind']
+    },
+]
+
+export default twScratchPrj
