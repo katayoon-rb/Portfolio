4bcf18856aa3bdbbc54ddc2604c08c3016af10f5
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-26 12:43:07 +0330

    Update userContributionToGrid.jsx

--- src/components/ContributionGrid/userContributionToGrid.jsx
+++ src/components/ContributionGrid/userContributionToGrid.jsx
@@ -1,5 +1,3 @@
-import { config } from "dotenv"
-config({ path: __dirname + "/../../../.env" })
 import { getGithubUserContribution } from './UserContribution.ts'
 
 const data = await getGithubUserContribution('katayoon-rb', {
