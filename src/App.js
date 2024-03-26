69bafabbdf401260f813f2f2869a78e5cb6579b8
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-26 20:50:45 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import Header from './components/Header.jsx'
 import Banner from './components/Banner.jsx'
 import AboutMe from './components/AboutMe.jsx'
 import Skills from './components/Skills.jsx'
 import Experiences from './components/Experiences.jsx'
 import Courses from './components/Courses.jsx'
+import Projects from './components/Projects.jsx'
 import Certificates from './components/Certificates.jsx'
 import Footer from './components/Footer.jsx'
+
+import RouteHeader from './components/RouteHeader.jsx'
+import AllCourses from './components/Courses/AllCourses.jsx'
+import AllProjects from './components/Projects/AllProjects.jsx'
 
 import profile from './images/profile.jpg'
 
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Banner />
-      <AboutMe profileLink={profile} />
-      <Skills />
-      <Experiences />
-      <Courses />
-      <Certificates />
-      <Footer />
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={
+            <div className="App">
+              <Header />
+              <Banner />
+              <AboutMe profileLink={profile} />
+              <Skills />
+              <Experiences />
+              <Courses />
+              <Projects />
+              <Certificates />
+            </div>
+          } />
+          <Route path='/courses' element={
+            <>
+              <RouteHeader />              
+              <AllCourses />
+            </>
+          } />
+          <Route path='/projects' element={
+            <>
+              <RouteHeader />              
+              <AllProjects />
+            </>
+          } />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
   )
 }
 
