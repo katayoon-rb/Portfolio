ad8772644b2509e8530fb3281d02561412c19ecf
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-26 20:54:24 +0330

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,16 +1,21 @@
 import CourseItem from './CourseItem.jsx'
 import coursesArr from '../data/courses.js'
 
+let latestCourses = []
+for (let i = 0; i < 6; i++) {
+    latestCourses.push(coursesArr[i])
+}
+
 function Courses() {
     return (
-        <div id="courses" className='py-10 pb-20'>
+        <div id="courses" className='py-16 pb-20 text-center'>
             <h3 className="font-Truculenta text-dark mb-10
-                    text-5xl md:text-6xl text-center">
+                    text-5xl md:text-6xl">
                 Courses
             </h3>
-            <div className='grid gap-7 px-10 md:px-20
+            <div className='grid gap-10 px-10 md:px-28
                     md:grid-cols-2 lg:grid-cols-3'>
-                {coursesArr.map(course => (
+                {latestCourses.map(course => (
                     <CourseItem
                         key={course.Title}
                         title={course.Title}
@@ -20,6 +25,11 @@ function Courses() {
                     />
                 ))}
             </div>
+            <a href='/courses'>
+                <button className='btn my-10 px-6 py-3 font-bold'>
+                    View More Courses
+                </button>
+            </a>
         </div>
     )
 }
