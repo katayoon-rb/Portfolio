06ccb30c49bb27cd7b6095dd91e1e54642d0213d
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 20:30:39 +0330

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,6 @@
-import SkillItem from './SkillItem.jsx'
 import skillsArr from '../data/skills.js'
+
+let stateAll = ["Expert", "Advanced", "Intermediate", "Basic"]
 
 function Skills() {
     return (
@@ -8,14 +9,21 @@ function Skills() {
                     text-5xl md:text-6xl text-center">
                 Skills
             </h3>
-            <div className='flex flex-wrap justify-center gap-5
-                    px-2 md:px-20'>
-                {skillsArr.map(skill => (
-                    <SkillItem
-                        key={skill.name}
-                        name={skill.name}
-                        state={skill.state}
-                    />
+            <div className='flex flex-wrap justify-between
+                    px-2 md:px-60'>
+                {stateAll.map(state => (
+                    <div className='bg-white rounded-lg text-center px-4 py-5
+                            min-w-[200px]'>
+                        <h5 className='font-Truculenta font-bold text-green mb-3
+                                border-b-2 border-green text-2xl md:text-3xl'>
+                            {state}
+                        </h5>
+                        {skillsArr.filter(s => s.state === state).map(skill => (
+                            <div key={skill.name} className="py-1">
+                                {skill.name}
+                            </div>
+                        ))}
+                    </div>
                 ))}
             </div>
         </div>
