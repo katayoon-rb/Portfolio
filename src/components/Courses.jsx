6de071dc69a0ce3f3f2b91cedf9360a885cdb7fe
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 19:51:52 +0330

    Update Courses.jsx

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -8,7 +8,7 @@ for (let i = 0; i < 6; i++) {
 
 function Courses() {
     return (
-        <div id="courses" className='py-20 text-center'>
+        <div id="courses" className='pt-20 text-center'>
             <h3 className="font-Truculenta text-dark mb-10
                     text-5xl md:text-6xl">
                 Courses
