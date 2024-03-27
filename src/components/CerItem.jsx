f234f87f68241599f9c0b2fac65ad9974ce4c9f4
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 20:02:08 +0330

    Update CerItem.jsx

--- src/components/CerItem.jsx
+++ src/components/CerItem.jsx
@@ -1,14 +1,16 @@
-function CerItem({name, image}) {
+function CerItem({name, image, link}) {
     return (
-        <div className="bg-white px-10 py-8 rounded-lg text-center w-[300px]">
-            <div className="font-Truculenta font-bold text-green text-2xl">
-                {name}
-            </div>
+        <a href={link}>
+            <div className="bg-white px-10 py-8 rounded-lg text-center w-[300px]">
+                <div className="font-Truculenta font-bold text-green text-2xl">
+                    {name}
+                </div>
 
-            <div className="font-medium text-gray-dark text-base md:text-md">
-                <img src={image} alt={name} className="w-full rounded-lg mt-3" />
+                <div className="font-medium text-gray-dark text-base md:text-md">
+                    <img src={image} alt={name} className="w-full mt-3" />
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
 
