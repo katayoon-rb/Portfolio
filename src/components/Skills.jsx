5bb40be63c6cb18ed8ef759b73edc68b04402ca2
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 21:06:31 +0330

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,8 +9,7 @@ function Skills() {
                     text-5xl md:text-6xl text-center">
                 Skills
             </h3>
-            <div className='flex flex-wrap justify-between
-                    px-2 md:px-60'>
+            <div className='grid px-20 md:grid-cols-2 lg:grid-cols-4 gap-x-16 gap-y-10'>
                 {stateAll.map(state => (
                     <div className='bg-white rounded-lg text-center px-4 py-5
                             min-w-[200px]'>
