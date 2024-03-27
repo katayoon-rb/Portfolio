9270df6e835c902b8eee323f73a1b2c38e4b325a
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 20:02:42 +0330

    Update certificates.js

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -2,18 +2,22 @@ let certificatesArr = [
     {
         'name': 'CS50x Harvard',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/CS50x.png?raw=true',
+        'link': 'https://cs50.harvard.edu/certificates/8c1603e7-97df-4595-9f4e-5c49c6184676'
     },
     {
         'name': 'CS50p Harvard',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/CS50p.png?raw=true',
+        'link': 'https://certificates.cs50.io/6567aba5-9eda-480e-a000-36f9a5bed67a'
     },
     {
         'name': '50 Projects In 50 Days',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/50%20Projects%20In%2050%20Days.jpg?raw=true',
+        'link': 'https://www.udemy.com/certificate/UC-d5a283ed-9a03-4048-9bf2-f972c04d9c5b/'
     },
     {
         'name': 'NodeJs Udemy',
         'image': 'https://github.com/katayoon-rb/Portfolio/blob/main/src/images/NodeJs.jpg?raw=true',
+        'link': 'https://www.udemy.com/certificate/UC-2caf8b66-0358-42f8-bc0b-e5a1c176a308/'
     },
 ]
 
