a256405d6004f25291d5a03f51e0bc4285c1b420
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-03-27 19:52:28 +0330

    Update Certificates.jsx

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -3,7 +3,7 @@ import certificatesArr from '../data/certificates.js'
 
 function Certificates() {
     return (
-        <div id="certificates" className='pt-3 pb-20'>
+        <div id="certificates" className='py-20'>
             <h3 className="font-Truculenta text-dark mb-10
                     text-5xl md:text-6xl text-center">
                 Certificates
