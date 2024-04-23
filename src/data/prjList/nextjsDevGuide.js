5cf823a1b3b5531244bb78c350ffc4987245dfd8
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-04-23 09:48:20 +0200

    Add files via upload

---
+++ src/data/prjList/nextjsDevGuide.js
@@ -0,0 +1,14 @@
+const nextjsDevGuidePrj = [
+  {
+    name: "CloudCorp",
+    link: "https://katycloudcorp.vercel.app/",
+    lan: ["typescript", "react", "nextjs"],
+  },
+  {
+    name: "Snippets",
+    link: "https://katysnippets.vercel.app/",
+    lan: ["typescript", "react", "nextjs"],
+  },
+];
+
+export default nextjsDevGuidePrj;
