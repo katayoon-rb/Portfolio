e5b2df98d0fc7395ac80babe3a4070c9f18260bd
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-06-25 12:20:42 +0330

    Add files via upload

---
+++ src/data/prjList/nextjsCG.js
@@ -0,0 +1,44 @@
+const nextjsCG = [
+  {
+    name: "NextLevelFood",
+    link: "https://katynextlevelfood.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextEvents",
+    link: "https://katynextevents.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextBlog",
+    link: "https://katynextblog.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextAuth",
+    link: "https://katynextauth.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextReactMeetups",
+    link: "https://katynextreactmeetups.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextNews",
+    link: "https://katynextnews.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextPosts",
+    link: "https://katynextposts.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+  {
+    name: "NextSports",
+    link: "https://katynextsports.vercel.app/",
+    lan: ["html", "css", "javascript", "next.js"],
+  },
+];
+
+export default nextjsCG;
