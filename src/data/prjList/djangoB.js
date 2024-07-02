d8ad287742073340efd03c71b29788ef1f294c4f
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-02 19:47:20 +0330

    Add files via upload

---
+++ src/data/prjList/djangoB.js
@@ -0,0 +1,29 @@
+const djangoB = [
+  {
+    name: "Django Movies",
+    link: "https://katydjbmovies.vercel.app/",
+    lan: ["html", "tailwind", "django", "python"],
+  },
+  {
+    name: "Django Job Board",
+    link: "https://katydjbjobboard.vercel.app/",
+    lan: ["html", "tailwind", "django", "python"],
+  },
+  {
+    name: "Django Link Shortener",
+    link: "https://katydjblinkshortener.vercel.app/",
+    lan: ["html", "tailwind", "django", "python"],
+  },
+  {
+    name: "Django Link Plant",
+    link: "https://katydjblinkplant.vercel.app/",
+    lan: ["html", "tailwind", "django", "python"],
+  },
+  {
+    name: "Django Trip Track",
+    link: "https://katydjbtriptrack.vercel.app/",
+    lan: ["html", "tailwind", "django", "python"],
+  },
+];
+
+export default djangoB;
