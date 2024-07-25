9321ff4577a8f1f6548abaa6373bf9ae2199f5f7
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-25 17:04:42 +0330

    Add files via upload

---
+++ src/data/prjList/nuxtB.js
@@ -0,0 +1,34 @@
+const NuxtBPrj = [
+    {
+        'name': 'Baby Name Generator Nuxt.js',
+        'link': 'https://katybabynamenuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+    {
+        'name': 'Top Restaurants Nuxt.js',
+        'link': 'https://katytoprestaurantsnuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+    {
+        'name': 'Dark Mode Nuxt.js',
+        'link': 'https://katydarkmodenuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+    {
+        'name': 'Weather App Nuxt.js',
+        'link': 'https://katyweatherappnuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+    {
+        'name': 'Todo App Nuxt.js',
+        'link': 'https://katytodoappnuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+    {
+        'name': 'Notes App Nuxt.js',
+        'link': 'https://katynotesappnuxtb.vercel.app/',
+        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+    },
+]
+
+export default NuxtBPrj
