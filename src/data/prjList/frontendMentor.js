c95b3b93b047bee65621c3b0b40e27d96421bd83
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:37:38 +0330

    Update frontendMentor.js

--- src/data/prjList/frontendMentor.js
+++ src/data/prjList/frontendMentor.js
@@ -2,17 +2,17 @@ const frontendMentorPrj = [
     {
         'name': 'Sunnyside Agency Landing Page',
         'link': 'https://katyfrontendmentorchallenges.netlify.app/03%20sunnyside%20agency%20landing%20page/index.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Profile Card',
         'link': 'https://katyfrontendmentorchallenges.netlify.app/02%20profile%20card/index.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     },
     {
         'name': 'Product Preview Card',
         'link': 'https://katyfrontendmentorchallenges.netlify.app/01%20product%20preview%20card/index.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     }
 ]
 
