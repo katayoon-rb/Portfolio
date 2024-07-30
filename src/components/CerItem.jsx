9ddd56f20c65843e30e747c40666a0080032c13e
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:22:21 +0330

    Update CerItem.jsx

--- src/components/CerItem.jsx
+++ src/components/CerItem.jsx
@@ -1,11 +1,10 @@
 function CerItem({name, image, link}) {
     return (
         <a href={link}>
-            <div className="bg-white px-10 py-8 rounded-lg text-center w-[300px]">
+            <div className="bg-white px-10 py-8 rounded-lg text-center w-[300px] h-[100%] flex flex-col justify-between">
                 <div className="font-Truculenta font-bold text-green text-2xl">
                     {name}
                 </div>
-
                 <div className="font-medium text-gray-dark text-base md:text-md">
                     <img src={image} alt={name} className="w-full mt-3" />
                 </div>
