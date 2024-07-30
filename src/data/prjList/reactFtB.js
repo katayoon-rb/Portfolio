dd81011103752af24bf3500684b2bca856be9702
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:40:07 +0330

    Update reactFtB.js

--- src/data/prjList/reactFtB.js
+++ src/data/prjList/reactFtB.js
@@ -2,17 +2,17 @@ const reactFtBPrj = [
     {
         'name': 'Feedback App',
         'link': 'https://katyfeedbackapp.vercel.app/',
-        'lan': ['html', 'css', 'javascript', 'react']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'React']
     },
     {
         'name': 'Github Finder App',
         'link': 'https://katygithubfinder.vercel.app/',
-        'lan': ['html', 'css', 'javascript', 'react']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'React']
     },
     {
         'name': 'House Marketplace',
         'link': 'https://katyhousemarketplace.vercel.app/',
-        'lan': ['html', 'css', 'javascript', 'react']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'React']
     }
 ]
 
