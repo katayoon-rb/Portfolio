9ac24d96708b68ec4ee675ec86bc944a37ede687
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:43:36 +0330

    Update vueComplete.js

--- src/data/prjList/vueComplete.js
+++ src/data/prjList/vueComplete.js
@@ -2,87 +2,87 @@ const vueCompletePrj = [
     {
         'name': 'Vue.js Events',
         'link': 'https://katyvueguide.netlify.app/02%20Basics%20&%20Core%20Concept/02%20Events/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Vue.js Conditional Content and Lists',
         'link': 'https://katyvueguide.netlify.app/03%20Conditional%20Content%20&%20Lists/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'The Monster Slayer Game',
         'link': 'https://katyvueguide.netlify.app/04%20The%20Monster%20Slayer%20Game%20Prj/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'My Friends',
         'link': 'https://katyvueguide.netlify.app/08%20component%20communication/01%20my%20friends/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Select a Topic',
         'link': 'https://katyvueguide.netlify.app/08%20component%20communication/02%20select%20a%20topic/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'The Learning Resources App',
         'link': 'https://katyvueguide.netlify.app/10%20The%20Learning%20Resources%20App%20Prj/dist/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Forms',
         'link': 'https://katyvueguide.netlify.app/11%20Forms/dist/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Applications',
         'link': 'https://katyvueguide.netlify.app/teams',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Animations and Transitions',
         'link': 'https://katyvueguide.netlify.app/14%20Animations%20and%20Transitions/dist/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Vuex Challenge',
         'link': 'https://katyvueguide.netlify.app/products',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Find a Coach',
         'link': 'https://katyvueguide.netlify.app/coaches',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Active Users - Composition API',
         'link': 'https://katyvueguide.netlify.app/19%20The%20Composition%20API/02%20Active%20Users/dist/index.html',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Routing Project',
         'link': 'https://katyvueguide.netlify.app/products',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Vuex Project',
         'link': 'https://katyvueguide.netlify.app/19%20the%20composition%20api/04%20vuex%20project/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Users with Mixins',
         'link': 'https://katyvueguide.netlify.app/20%20reusing%20functionality/01%20mixins/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Users with Hooks',
         'link': 'https://katyvueguide.netlify.app/20%20reusing%20functionality/02%20hooks/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     },
     {
         'name': 'Active Users - Reusing',
         'link': 'https://katyvueguide.netlify.app/20%20reusing%20functionality/03%20active%20users/dist/',
-        'lan': ['html', 'css', 'javascript', 'vue']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Vue.js']
     }
 ]
 
