8e60fa440bf16100dd24b9464166710ef7e45139
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:41:00 +0330

    Update nuxtB.js

--- src/data/prjList/nuxtB.js
+++ src/data/prjList/nuxtB.js
@@ -2,32 +2,32 @@ const nuxtBPrj = [
     {
         'name': 'Baby Name Generator Nuxt.js',
         'link': 'https://katybabynamenuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
     {
         'name': 'Top Restaurants Nuxt.js',
         'link': 'https://katytoprestaurantsnuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
     {
         'name': 'Dark Mode Nuxt.js',
         'link': 'https://katydarkmodenuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
     {
         'name': 'Weather App Nuxt.js',
         'link': 'https://katyweatherappnuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
     {
         'name': 'Todo App Nuxt.js',
         'link': 'https://katytodoappnuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
     {
         'name': 'Notes App Nuxt.js',
         'link': 'https://katynotesappnuxtb.vercel.app/',
-        'lan': ['vue', 'nuxt.js', 'css', 'typescript']
+        'lan': ['Vue.js', 'Nuxt.js', 'CSS', 'TypeScript']
     },
 ]
 
