d3bec29d91acad6d3121917df395b8362b72117a
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:41:30 +0330

    Update scrimba.js

--- src/data/prjList/scrimba.js
+++ src/data/prjList/scrimba.js
@@ -2,72 +2,72 @@ const scrimbaPrj = [
     {
         'name': 'My Travel Journal',
         'link': 'https://bespoke-frangollo-6f7482.netlify.app/',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Portfolio Card',
         'link': 'https://steady-figolla-eb5060.netlify.app/',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Movie Watchlist',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/11%20Movie%20Watchlist/src/movie-watchlist.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Color Scheme Generator',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/10%20Color%20Scheme%20Generator/color-scheme.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Tindog',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/09%20Tindog/src/tindog.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Restaurant Ordering App',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/09%20Restaurant%20Ordering%20App/restaurant-ordering-app.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Oldagram',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/08%20Oldagram/oldagram.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Unit Converter',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/07%20Unit%20Converter/unit-converter.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Password Generator Scrimba',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/06%20Password%20Generator/password-generator.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Basketball Soreboard',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/05%20Basketball%20Soreboard/scoreboard.html',
-        'lan': ['html', 'css', 'javascript']
+        'lan': ['HTML', 'CSS', 'JavaScript']
     },
     {
         'name': 'Visit Amsterdam',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/04%20Visit%20Amsterdam/visit-amsterdam.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     },
     {
         'name': '0B-Day',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/03%20B-Day/b-day.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     },
     {
         'name': 'Business Card',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/02%20Business%20Card/busi-card.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     },
     {
         'name': 'Personal Website',
         'link': 'https://katayoon-rb.github.io/Fronend-Path-Scrimba/01%20Personal%20Website/personal-website.html',
-        'lan': ['html', 'css']
+        'lan': ['HTML', 'CSS']
     }
 ]
 
