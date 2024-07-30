2272c8b7a021a933198e8eff2b33c93a54724513
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:40:18 +0330

    Update reactTs.js

--- src/data/prjList/reactTs.js
+++ src/data/prjList/reactTs.js
@@ -2,28 +2,27 @@ const ReactTsPrj = [
     {
         'name': 'GoalList',
         'link': 'https://katyreacttypescript.netlify.app/01-GoalList/index.html',
-        'lan': ['html', 'css', 'javascript', 'typescript']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'TypeScript']
     },
     {
         'name': 'AdvComponents',
         'link': 'https://katyreacttypescript.netlify.app/02-AdvComponents/index.html',
-        'lan': ['html', 'css', 'javascript', 'typescript']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'TypeScript']
     },
     {
         'name': 'ReactTimer',
         'link': 'https://katyreacttypescript.netlify.app/03-ReactTimer/index.html',
-        'lan': ['html', 'css', 'javascript', 'typescript']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'TypeScript']
     },
     {
         'name': 'DataFetching',
         'link': 'https://katyreacttypescript.netlify.app/04-DataFetching/index.html',
-        'lan': ['html', 'css', 'javascript', 'typescript']
-    }
-    ,
+        'lan': ['HTML', 'CSS', 'JavaScript', 'TypeScript']
+    },
     {
         'name': 'ReduxShop',
         'link': 'https://katyreacttypescript.netlify.app/05-ReduxShop/index.html',
-        'lan': ['html', 'css', 'javascript', 'typescript']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'TypeScript']
     }
 ]
 
