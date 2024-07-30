1d56729811fcffb742c7a89bb228413e70040a8d
Katayoon Rabbani <55879886+katayoon-rb@users.noreply.github.com>
2024-07-30 18:42:53 +0330

    Update twScratch.js

--- src/data/prjList/twScratch.js
+++ src/data/prjList/twScratch.js
@@ -2,42 +2,42 @@ const twScratchPrj = [
     {
         'name': 'Sandbox',
         'link': 'https://katytailwindfromscratch.netlify.app/00%20Sandbox/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Joy of React Mini Projects',
         'link': 'https://katytailwindfromscratch.netlify.app/05%20-%20Mini-Projects/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Clipboard Website',
         'link': 'https://katytailwindfromscratch.netlify.app/06%20-%20Clipboard%20Website/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'LoopStudios Website',
         'link': 'https://katytailwindfromscratch.netlify.app/07%20-%20Loopstudios%20Website/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Shortly Website',
         'link': 'https://katytailwindfromscratch.netlify.app/08%20-%20Shortly%20Website/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Testimonial Grid',
         'link': 'https://katytailwindfromscratch.netlify.app/09%20-%20Testimonial%20Grid%20Project/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Fylo Website',
         'link': 'https://katytailwindfromscratch.netlify.app/10%20-%20Fylo%20Website/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
     {
         'name': 'Bookmark Manager Website',
         'link': 'https://katytailwindfromscratch.netlify.app/11%20-%20Bookmark%20Manager%20Website/index.html',
-        'lan': ['html', 'css', 'javascript', 'tailwind']
+        'lan': ['HTML', 'CSS', 'JavaScript', 'Tailwind']
     },
 ]
 
